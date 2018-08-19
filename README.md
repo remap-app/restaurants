86d8ee133c0686e57558eec00990479bc2597718
sugarshin <shinsugar@gmail.com>
2018-08-19 22:29:50 +0900

    Normalize response

--- README.md
+++ README.md
@@ -1,12 +1,38 @@
 # restaurants
 
-ReMap Restaurants API Microservice
+ReMap Restaurants Microservice
 
 |        | Status |
 | :----: | :----: |
 | master | [![CircleCI](https://circleci.com/gh/remap-app/restaurants/tree/master.svg?style=svg&circle-token=086672baf7498fc5cae959a3e715f25bc28d0b49)](https://circleci.com/gh/remap-app/restaurants/tree/master) |
 | deployment/staging | [![CircleCI](https://circleci.com/gh/remap-app/restaurants/tree/deployment%2Fstaging.svg?style=svg&circle-token=086672baf7498fc5cae959a3e715f25bc28d0b49)](https://circleci.com/gh/remap-app/restaurants/tree/deployment%2Fstaging) |
 | deployment/production | [![CircleCI](https://circleci.com/gh/remap-app/restaurants/tree/deployment%2Fproduction.svg?style=svg&circle-token=086672baf7498fc5cae959a3e715f25bc28d0b49)](https://circleci.com/gh/remap-app/restaurants/tree/deployment%2Fproduction) |
+
+## Response
+
+```typescript
+interface IResponse {
+  id: string;
+  name: string;
+  name_kana: string;
+  latitude: string;
+  longitude: string;
+  url: string;
+  url_mobile?: string;
+  images: string[];
+  coupon_url: { desktop?: string, mobile?: string };
+  tel?: string;
+  opening_times?: string;
+  catchphrase?: string;
+  description: string;
+  access?: string;
+  holiday?: string;
+  credit_card?: string;
+  non_smoking?: string;
+  lunch?: string;
+  children?: string;
+}
+```
 
 ## Endpoints
 
--- helpers.js
+++ helpers.js
@@ -1,4 +1,9 @@
+const mapObj = require('map-obj')
 const uniqWith = require('lodash.uniqwith')
+const get = require('lodash.get')
+const isPlainObject = require('lodash.isplainobject')
+const isEmpty = require('lodash.isempty')
+const head = require('lodash.head')
 
 module.exports.uniqRestaurants = restaurants => uniqWith(restaurants, (a, b) => {
   const aLat = a && (a.lat || a.latitude)
@@ -9,3 +14,53 @@ module.exports.uniqRestaurants = restaurants => uniqWith(restaurants, (a, b) => 
 })
 module.exports.isHotpepper = id => /^J\d{9}$/.test(id)
 module.exports.isGurunavi = id => /^[a-zA-z0-9]{7}$/.test(id)
+
+const mapEntity = (entity, map) => {
+  return Object.entries(map).reduce(
+    (ret, [key, value]) => {
+      let resultVal = null
+      if (value === true) {
+        const path = key
+        resultVal = get(entity, path, null)
+      } else if (typeof value === 'string') {
+        const result = value.split(',').map(path => get(entity, path, ''))
+        resultVal = result.length === 1
+          ? head(result)
+          : result.filter(r => !!r).join(' ')
+      } else if (Array.isArray(value)) {
+        resultVal = value.map(path => get(entity, path, null))
+      } else if (isPlainObject(value)) {
+        resultVal = Object.entries(value).reduce(
+          (ret, [k, path]) => ({ ...ret, [k]: get(entity, path, null) }),
+          {}
+        )
+      }
+      return { ...ret, [key]: resultVal }
+    },
+    {}
+  )
+}
+
+const mapEntities = (entities, map) => {
+  return entities.map(entity => mapEntity(entity, map))
+}
+
+module.exports.normalizeRestaurants = (restaurants, map) => {
+  return Array.isArray(restaurants)
+    ? mapEntities(restaurants, map)
+    : mapEntity(restaurants, map)
+}
+
+const normalizeNullValue = object => {
+  return mapObj(
+    object,
+    (key, value) => (isPlainObject(value) && isEmpty(value)) ? [key, null] : [key, value],
+    { deep: true }
+  )
+}
+
+module.exports.normalizeNullValues = restaurants => {
+  return Array.isArray(restaurants)
+    ? restaurants.map(normalizeNullValue)
+    : normalizeNullValue(restaurants)
+}
