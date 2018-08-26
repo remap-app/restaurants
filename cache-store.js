06b364a3c10864fa65192f61bf4f022993ec2b6a
sugarshin <shinsugar@gmail.com>
2018-08-26 11:36:34 +0900

    Tweak env

--- cache-store.js
+++ cache-store.js
@@ -2,12 +2,12 @@ const Keyv = require('keyv')
 const KeyvRedis = require('@keyv/redis')
 const ms = require('ms')
 
-const { NODE_ENV, CACHE_DATABASE_PASSWORD, CACHE_DATABASE_HOSTNAME } = process.env
+const { NODE_ENV, CACHE_DATABASE_PASSWORD, CACHE_DATABASE_HOSTNAME, CACHE_DATABASE_PORT } = process.env
 
-const REDIS_HOSTNAME = NODE_ENV === 'production'
+const HOSTNAME = NODE_ENV === 'production'
   ? `:${CACHE_DATABASE_PASSWORD}@${CACHE_DATABASE_HOSTNAME}`
   : CACHE_DATABASE_HOSTNAME
 
-const store = new Keyv({ store: new KeyvRedis(`redis://${REDIS_HOSTNAME}`), ttl: ms('1d') })
+const store = new Keyv({ store: new KeyvRedis(`redis://${HOSTNAME}:${CACHE_DATABASE_PORT}`), ttl: ms('1d') })
 
 module.exports = store
