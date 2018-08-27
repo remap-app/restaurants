d5ef4488a34d037ffccf16a970af2543989d3dd3
sugarshin <shinsugar@gmail.com>
2018-08-27 19:09:02 +0900

    Refactoring

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 require('dotenv').config()
 
-const { STATUS_CODES } = require('http')
 const { router, get } = require('microrouter')
 const compose = require('micro-compose')
-const { handleErrors, createError } = require('micro-errors')
+const { handleErrors } = require('micro-errors')
 const cors = require('micro-cors-multiple-allow-origin')
 const UrlPattern = require('url-pattern')
-const rootHandler = require('./handlers/root')
-const idHandler = require('./handlers/id')
+const getList = require('./routes/getList')
+const getById = require('./routes/getById')
 
 module.exports = compose(
   handleErrors({ debug: process.env.NODE_ENV !== 'production' }),
@@ -17,10 +16,7 @@ module.exports = compose(
   })
 )(
   router(
-    get('/', rootHandler),
-    get(new UrlPattern(/^\/([\w-]+)$/, ['id']), idHandler),
-    get('/*', () => {
-      throw createError(404, STATUS_CODES[404])
-    })
+    get('/', getList),
+    get(new UrlPattern(/^\/([\w-]+)$/, ['id']), getById)
   )
 )
